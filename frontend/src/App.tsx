91d66835f6eaf9b153d4e8ee848b8d42e3b0427b
Tiago Almeida <tiagodalmeida87@hotmail.com>
2022-07-11 16:04:07 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h2>Olá mundo!!</h2>
-      <NotificationButton />
+      <Header />
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+
     </>
   )
 }
